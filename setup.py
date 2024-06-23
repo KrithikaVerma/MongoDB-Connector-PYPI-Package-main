1d361863d8f960a5a7b3b18075d54626ebf353e8
KrithikaVerma <70284201+KrithikaVerma@users.noreply.github.com>
2024-06-23 19:36:10 -0400

    Code updated with different package name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "Mongo-Connect" # This package name will be displayed on PyPI repository
+PKG_NAME= "Mongo-Connect-Kritz" # This package name will be displayed on PyPI repository
 AUTHOR_USER_NAME = "KrithikaVerma"
 AUTHOR_EMAIL = "krithika.verma@gmail.com"
 
